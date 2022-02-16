9982a765142251d83a17ff1dcffc4151ea744434
Camila Abreu <81877746+mila-developer@users.noreply.github.com>
2022-02-16 23:03:08 +0000

    ADD: create react app

--- src/redux/root-reducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-
-import userReducer from "./user/user.reducer";
-import cartReducer from "./cart/cart-reducer";
-
-export default combineReducers({
-    user: userReducer,
-    cart: cartReducer
-});
